87278ad4a0ebf62f4d9f8a48a869d5c09d513a4e
Alexandre <alexandre.faria95@gmail.com>
2016-11-02 16:19:20 -0200

    Funcoes de alocacao ainda n funcionando

--- arrdir.h
+++ arrdir.h
@@ -3,32 +3,51 @@
 #include <time.h>
 
 //DEFINIÇÃO DOS TIPOS.
-typedef struct arrdir{
-	int tipo; //Tipo Arquivo==1 Tipo Diretorio==0
-	struct arrdir *filho, *prox_irmao;
-	void *info;
-}TAD;
+	typedef struct arrdir{
+		int tipo; //Tipo Arquivo==1 Tipo Diretorio==0
+		struct arrdir *filho, *prox_irmao;
+		void *info;
+	}TAD;
 
-typedef struct dir{
-	char nome[50] ;
-	const time_t criacao ;
-	int permissoes;
-	int num_arq;
-}TDIR;
+	typedef struct dir{	
+		char * nome; 
+		const time_t criacao = time(NULL);
+		int perm;
+		int num_arq = 0;
+	}TDIR;
 
-typedef struct arq{
-	char nome[50] ;
-	int tipo; //Texto ou Binario
-	int permissoes;
-	const time_t criacao;
-	time_t ult_att;
-};
+	typedef struct arq{
+		char * nome; // = (char *) malloc (sizeof (char * 25)
+		int tipo; //Texto ou Binario
+		int perm;
+		const time_t criacao = time(NULL);
+		char * ult_att;
+	}TARQ;
 
 
-//METODOS SOBRE A ARVORE. 
+//METODOS SOBRE A ARVORE.
 
+	TAD * aloca_no (char* nome ,int tipo, int permissao){
+		TAD * novo = (TAD *) malloc (sizeof (TAD) );
+		novo->tipo = tipo;
+		novo->filho = novo->prox_irmao = NULL;
+		//Refenciar a info
+		return novo;
+	}
+	
 //METODOS SOBRE ARQUIVOS E DIRETORIOS.
 
 //METODOS SOBRE DIRETORIO.
-
+	TDIR * aloca_dir(char* nome, int permissao){
+		TDIR * novo = (TDIR *)malloc (sizeof(TDIR));
+		novo->nome = nome;
+		novo->perm = permissao;
+	}
 //METODOS SOBRE ARQUIVOS.
+	TARQ * aloca_arq(char* nome, int permissao){
+		TARQ * novo = (TARQ *) malloc (sizeof(TARQ));
+		novo->nome = nome;
+		novo->perm = permissao;
+		
+		return novo;
+	}
--- main.c
+++ main.c
@@ -3,7 +3,34 @@
 #include "arrdir.h"
 
 int main(int argc, char *argv[]) {
+	//Teste da função de imprimir a hora do sistema.
 	const time_t timer = time(NULL);
 	printf("%s\n", ctime(&timer));	
+	
+	int comando;
+	do{
+		//Receber o comando do usuario pelo menos uma vez
+		printf("Digite um dos comandos listados: \n");
+		printf("0 - Sair ");
+		
+		scanf("%d", &comando);
+		
+		switch(comando){
+			case 0:
+				break;
+			case 1:
+				//Comando 1
+				break;
+			case 2:
+				//Comando 2	
+				break;
+			default:
+				printf("Comando inválido, por favor entre com um comando númerico existente.");
+				break;
+		}
+		
+		
+	}while(comando != 0);
+	//Liberar memoria da arvore. Inclusive Raiz.
 	return 0;
 }
